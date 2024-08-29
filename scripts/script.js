71ff7d4faab2ceccfbfcaa8071637085434816b2
AetherPhx <aetherphx@gmail.com>
2024-08-29 17:09:47 -0500

    Implement toggle Nav Menu

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+const prepareNavMenu = () => {
+	const nav = document.querySelector(".nav");
+
+	nav.addEventListener("click", (event) => {
+		console.log(`Se presionó el ${event.target.className}`);
+		event.target.matches(".nav__btn, .nav__icon, .nav__item, .nav__link")
+			? toogleNavMenu()
+			: null;
+	});
+};
+
+const toogleNavMenu = () =>
+	document.querySelector(".nav__menu").classList.toggle("nav__menu--hidden");
+
+const startApp = () => {
+	console.log("App Started");
+	prepareNavMenu();
+};
+
+startApp();
